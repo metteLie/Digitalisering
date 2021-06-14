35e17b3a775b932f2f283a417bdb51f64a486e97
Mette Lie <85258344+mettel99@users.noreply.github.com>
2021-06-14 19:27:33 +0200

    fauna update

--- digitalisering/utils/Fauna.js
+++ digitalisering/utils/Fauna.js
@@ -1,3 +1,20 @@
 const faunadb = require('faunadb');
 const faunaClient = new faunadb.Client({ secret: process.env.FAUNA_KEY});
 const q = fauabd.query;
+
+
+const getDokumenter = async() => {
+    const {data} = await faunaClient.query(
+        q.Map(
+            q.Paginate(q.Documents(q.Collection('dokumenter'))
+            ),
+            q.Lambda('ref', q.Get(q.Var('ref')))
+        )
+    );
+    const dokumenter = data.map(dokument => {
+        dokument.id = dokument.ref.id;
+        delete dokument.ref;
+        return dokument;
+    })
+};
+
