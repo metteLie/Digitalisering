2f8e42d5c0384d027f235763a55691c59b044d34
Mette Lie <85258344+mettel99@users.noreply.github.com>
2021-06-15 14:16:02 +0200

    oppsett for frontend

--- digitalisering/pages/input/index.js
+++ digitalisering/pages/input/index.js
@@ -1,3 +1,11 @@
 import Link from 'next/link';
 
+const registrer = () => {
+    return(
+        <div>
+            <h1>Kode her i return</h1>
+        </div>
+    )
+}
 
+export default registrer;
--- digitalisering/pages/output/index.js
+++ digitalisering/pages/output/index.js
@@ -1,3 +1,11 @@
 import Link from 'next/link';
 
+const rediger = () => {
+    return(
+        <div>
+            <h1>Kode her i return</h1>
+        </div>
+    )
+}
 
+export default rediger;
