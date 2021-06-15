6aa79906b45c86e6e570cea236c3d4a4f139a19e
Mette Lie <85258344+mettel99@users.noreply.github.com>
2021-06-15 16:54:59 +0200

    logo

--- digitalisering/pages/input/index.js
+++ digitalisering/pages/input/index.js
@@ -1,10 +1,27 @@
 import Link from 'next/link';
+import Head from 'next/head';
+
+export const getStaticProps = async () => {
+    const res = await fetch('http://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return { 
+        props: {ting: data}
+    }
+}
+// Fra youtube, tror ikke vi trenger dette^
 
 const registrer = () => {
     return(
-        <div>
+        <>
+        <Head>
+            <title>Profilteam | Registrer</title>
+            <meta name="keywords" content="registrering"/>
+        </Head>
+            <div>
             <h1>Kode her i return</h1>
         </div>
+        </>
     )
 }
 
