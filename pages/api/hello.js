a079cf0b49b96fc9616d25806a26eddb5d3c9673
Andkleven <amkleven@gmail.com>
2021-06-23 10:54:17 +0200

    anders

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,47 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import faunadb, { Client, Collection} from "faunadb"
+import faunadb, { Client, Collection } from "faunadb";
 import { Body } from "node-fetch";
-const client = new Client({ 
+const client = new Client({
   secret: "fnAEMFFhWoACBZJGJSlKVaVzdfzZwVxiauTfpjax",
 });
-const q = faunadb.query; 
+const q = faunadb.query;
 
-
-
-export default function handler (req, res)  {
-if (req.method === "POST"){
-  const data = JSON.parse(req.body);
-  client
-  .query(
-    q.Create(
-      q.Collection('oppgave2'),
-      {
-        data,
-      },
-    )
-  )
-  .then((ret) => {
-  res.status(200).json({...ret.data, id: ret.ref.id});
-  })
-  .catch((err) => res.status(400).json(err));
-
-  
-}else if (req.method=== "GET") {
-  client
-  .query(
-  q.Map(
-    q.Paginate(q.Documents(q.Collection("oppgave2"))),
-    q.Lambda((x)=> q.Get(x))
-  )
-  )
-  .then((ret)=> {
-    const newData = ret.data.map((d) => {
-      return {...ret.data, id: d.ref.id}
-    });
-    console.log(newData);
-    res.status(200).json(newData);
-  }) 
-  .catch((err) => res.status(400).json(err));
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const data = JSON.parse(req.body);
+    client
+      .query(
+        q.Create(q.Collection("oppgave2"), {
+          data,
+        })
+      )
+      .then((ret) => {
+        res.status(200).json({ ...ret.data, id: ret.ref.id });
+      })
+      .catch((err) => res.status(400).json(err));
+  } else if (req.method === "GET") {
+    client
+      .query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection("oppgave2"))),
+          q.Lambda((x) => q.Get(x))
+        )
+      )
+      .then((ret) => {
+        console.log(ret);
+        const newData = ret.data.map((d) => {
+          console.log(d);
+          return { ...d.data, id: d.ref.id };
+        });
+        console.log(newData);
+        res.status(200).json(newData);
+      })
+      .catch((err) => res.status(400).json(err));
+  }
 }
-};
