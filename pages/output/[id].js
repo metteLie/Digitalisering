dfe8bd4952e00765c110b2c59325f18a11ac1a6c
ilahnmbu <85282393+ilahnmbu@users.noreply.github.com>
2021-06-23 10:26:42 +0200

    dev

--- pages/output/[id].js
+++ pages/output/[id].js
@@ -1,41 +1,29 @@
-/* Side for ulike data 
-for gitt id */
+import useSWR, { mutate } from "swr";
 
-export const getStaticPaths = async () => {
-    const res = await fetch('http://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-
-    const paths = data.map(objekt => {
-        return {
-            params: { id: objekt.id.toString()}
-        }
-    })
-
-    return {
-        paths,
-        fallback: false
-    }
-}
-
-export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const res =  await fetch('http://jsonplaceholder.typicode.com/users' + id);
-    const data = await res.json();
-
-    return {
-        props: { objekt: data }
-    }
-}
-
-const sjekkliste = ({objekt}) => {
-    return ( 
-    <div>
-        <h1>Dette skal vise sjekklisten for { objekt.name } </h1>
-        <h2>Burde komme opp navn?
-            Merk igjen fetch må hentes fra fauna, 
-            dette er kun illustrasjon
-        </h2>
-    </div>  );
-}
- 
-export default sjekkliste;
+ export default function OnSubmit() { 
+    const { data } = useSWR(`/api/hello`, (url) =>
+    fetch(url).then((res) => res.json())
+    );
+console.log(data)
+return( <> {data ? (
+    <>
+      <TableHead>
+        <TableRow>
+          <TableCell align="right">Mottatt avviksmelding på forutgående arbeider </TableCell>
+          <TableCell align="right">Registrerte avvik på forutgående / tilstøtende konstruksjoner.</TableCell>
+          <TableCell align="right">Hvor i bygget er avviket:</TableCell>
+          <TableCell align="right">Avvik varslet:</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((row) => (
+          <TableRow key={row.id}>
+            <TableCell align="right">{row.test1}</TableCell>
+            <TableCell align="right">{row.test2}</TableCell>
+            <TableCell align="right">{row.test3}</TableCell>
+            <TableCell align="right">{row.test4}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </>
+  ) : null} </>)}
