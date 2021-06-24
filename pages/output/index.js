1cee631d2fab4139850a8359c777bd3edbbd3cbd
ilahnmbu <85282393+ilahnmbu@users.noreply.github.com>
2021-06-24 17:14:57 +0200

    Update index.js

--- pages/output/index.js
+++ pages/output/index.js
@@ -1,1 +1,13 @@
-
+import Head from 'next/head';
+ 
+const rediger = () => {
+    return(
+        <Head>
+            <title>Profilteam | Rediger</title>
+            <meta name="keywords" content="redigering"/>
+        </Head>
+        
+    );
+}
+ 
+export default rediger;
