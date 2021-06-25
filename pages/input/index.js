cb71e38f4a95c8f36727b767dcd9e9493c60bc20
Andkleven <amkleven@gmail.com>
2021-06-25 16:19:45 +0200

    delete

--- pages/input/index.js
+++ pages/input/index.js
@@ -1,24 +1,23 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import {skjema} from './oppgave1.js';
-import { useState } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import { skjema } from "./oppgave1.js";
 
 let punkt = [...Array(22).keys()];
-let kjente_avvik = punkt.slice(0,6);
+let kjente_avvik = punkt.slice(0, 6);
 
 const registrer = () => {
-    return(
-        <>
-        <Head>
-            <title>Profilteam | Registrer</title>
-            <meta name="keywords" content="registrering"/>
-        </Head>
-            <div>
-            <h1>Sjekkliste</h1>
-            {skjema()}
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Profilteam | Registrer</title>
+        <meta name="keywords" content="registrering" />
+      </Head>
+      <div>
+        <h1>Sjekkliste</h1>
+        {skjema()}
+      </div>
+    </>
+  );
+};
 
 export default registrer;
